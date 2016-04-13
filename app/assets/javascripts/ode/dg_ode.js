30a84ae1803336800574943e58e9f235506dccf0
musicmunky <musicmunky@gmail.com>
2016-04-13 15:09:07 -0400

    Update with new version of BigInt and links for diff eqs

---
+++ app/assets/javascripts/ode/dg_ode.js
@@ -0,0 +1,6 @@
+//LINKS TO LOOK AT FOR IMPLEMENTATION OF ODE JAVASCRIPT
+
+//http://tutorial.math.lamar.edu/Classes/DE/EulersMethod.aspx
+//http://mtdevans.com/2013/05/fourth-order-runge-kutta-algorithm-in-javascript-with-demo/
+//http://stackoverflow.com/questions/29830807/runge-kutta-problems-in-js
+//http://codeflow.org/entries/2010/aug/28/integration-by-example-euler-vs-verlet-vs-runge-kutta/
